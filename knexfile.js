e82d55b4c1ec3d25dbaa9492a31c8b98d958b0a2
Rodrigo De La Mora <delamorarodrigo3141@yahoo.com>
2020-05-21 22:56:48 -0700

    Made a test server and refactored everything the chris adams way using async.

--- knexfile.js
+++ knexfile.js
@@ -22,6 +22,24 @@ module.exports = {
     },
   },
 
+  testing: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: { filename: './data/test.db3' },
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: { 
+      directory: './data/seeds' 
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      }
+    },
+  },
+
   staging: {
     client: 'postgresql',
     connection: {
--- server.spec.js
+++ server.spec.js
@@ -4,9 +4,9 @@ const server = require('./server');
 
 const db = require('./data/dbConfig');
 
-afterAll(async() => {
-    await db('items').truncate();
-})
+beforeEach(() => {
+    return db.migrate.rollback().then(()=>db.migrate.latest()).then(()=>db.seed.run());
+});
 
 describe('server', () => {
     it('should run tests', () => {
@@ -14,125 +14,79 @@ describe('server', () => {
     })
 
     describe('GET /', () => {
-        it('should return http status code 200', () => {
-            return (
-                supertest(server)
-                    .get('/')
-                    .then(res => {
-                        expect(res.status).toBe(200)
-                    })
-            )
+        it('should return http status code 200', async() => {
+            const res = await supertest(server).get('/');
+            
+            expect(res.status).toBe(200)
         })
 
-        it('should return { api: "Up" }', () => {
-            return (
-                supertest(server)
-                    .get('/')
-                    .then(res => {
-                        expect(res.body).toEqual({ api: "Up" })
-                    })
-            )
+        it('should return { api: "Up" }', async() => {
+            const res = await supertest(server).get('/');
+
+            expect(res.body).toEqual({ api: "Up" })
         })
     })
 
     describe('GET /api/items', () => {
-        it('should return http status code 200', () => {
-            return (
-                supertest(server)
-                    .get('/api/items')
-                    .then(res => {
-                        expect(res.status).toBe(200)
-                    })
-            )
+        it('should return http status code 200', async() => {
+            const res = await supertest(server).get('/api/items');
+
+            expect(res.status).toBe(200)
         })
 
-        it('should return an array', () => {
-            return (
-                supertest(server)
-                    .get('/api/items')
-                    .then(res => {
-                        expect(Array.isArray(res.body.data)).toBe(true)
-                    })
-            )
+        it('should return an array', async() => {
+            const res = await supertest(server).get('/api/items');
+
+            expect(Array.isArray(res.body.data)).toBe(true)
         })
     })
 
 
     describe('POST /api/items', () => {
-        it('should return http status code 400 with nothing in the body', () => {
-            return (
-                supertest(server)
-                    .post('/api/items')
-                    .then(res => {
-                        expect(res.status).toBe(400)
-                    })
-            )
+        it('should return http status code 400 with nothing in the body', async() => {
+            const res = await supertest(server).post('/api/items');
+
+            expect(res.status).toBe(400)
         })
 
-        it('should return the object sent with an id of 1', () => {
-            return (
-                supertest(server)
-                    .post('/api/items')
-                    .send({ name: "Gold Sword", durability: 5, enhancement: 5 })
-                    .then(res => {
-                        expect(res.body.data).toEqual({ id: 1, name: "Gold Sword", durability: 5, enhancement: 5 })
-                    })
-            )
+        it('should return the object sent with an id of 1', async() => {
+            const res = await supertest(server).post('/api/items').send({ name: "Gold Sword", durability: 5, enhancement: 5 });
+
+            expect(res.body.data).toEqual({ id: 4, name: "Gold Sword", durability: 5, enhancement: 5 })
         })
 
-        it('should return http status code 201 with a valid object', () => {
-            return (
-                supertest(server)
-                    .post('/api/items')
-                    .send({ name: "Gold Sword", durability: 5, enhancement: 5 })
-                    .then(res => {
-                        expect(res.status).toBe(201)
-                    })
-            )
+        it('should return http status code 201 with a valid object', async() => {
+            const res = await supertest(server).post('/api/items').send({ name: "Gold Sword", durability: 5, enhancement: 5 });
+
+            expect(res.status).toBe(201)
         })
     })
 
     describe('GET /api/items/:id', () => {
-        it('should return http status code 200', () => {
-            return (
-                supertest(server)
-                    .get('/api/items/1')
-                    .then(res => {
-                        expect(res.status).toBe(200)
-                    })
-            )
+        it('should return http status code 200', async() => {
+            const res = await supertest(server).get('/api/items/1');
+
+            expect(res.status).toBe(200)
         })
 
-        it('should return http status code 404', () => {
-            return (
-                supertest(server)
-                    .get('/api/items/50')
-                    .then(res => {
-                        expect(res.status).toBe(404)
-                    })
-            )
+        it('should return http status code 404', async() => {
+            const res = await supertest(server).get('/api/items/50');
+
+            expect(res.status).toBe(404)
         })
     })
 
     describe('DELETE /api/items/:id', () => {
-        it('should return http status code 204', () => {
-            return (
-                supertest(server)
-                    .delete('/api/items/1')
-                    .then(res => {
-                        expect(res.status).toBe(204)
-                    })
-            )
+        it('should return http status code 204', async() => {
+            const res = await supertest(server).delete('/api/items/1');
+
+            expect(res.status).toBe(204)
         })
 
-        it('should return http status code 404 with nothing in the database', () => {
-            return (
-                supertest(server)
-                    .delete('/api/items/50')
-                    .then(res => {
-                        expect(res.status).toBe(404)
-                    })
-            )
+        it('should return http status code 404 with nothing in the database', async() => {
+            const res = await supertest(server).delete('/api/items/50');
+
+            expect(res.status).toBe(404)
         })
     })
 })
